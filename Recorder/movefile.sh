bf4a1d054ea1b40110270993fd8f1927806b27be
Chetan Tayal <chetantayal138@gmail.com>
2020-05-28 01:19:27 +0530

    Delete movefile.sh

--- Recorder/movefile.sh
+++
@@ -1,6 +0,0 @@
-echo "Shutting down node server"
-kill -9 $(ps aux | grep '\snode\s' | awk '{print $2}')
-exit
-
-~                                                                                                        
-~                                      h
